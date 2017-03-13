a557c2cdc93589f54f0bc72f9da50568a426e432
Ben Fifield <benfifield@gmail.com>
2017-03-13 16:09:28 -0400

    cleanup of builder

--- builder.sh
+++ builder.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Build documentation, compile C++ attributes
-R -e 'library(tools);sink("src/redist_init.c");package_native_routine_registration_skeleton(".");sink()'
-R -e 'library(devtools);document()'
+R -e 'sink("src/redist_init.c");tools::package_native_routine_registration_skeleton(".");sink()'
+R -e 'devtools::document()'
 R -e 'library(Rcpp);compileAttributes(verbose = TRUE)'
 
 # Clean up src folder before build
