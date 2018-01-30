121a62f360804fd495c10f0d6b521149805c45d0
Ben Fifield <benfifield@gmail.com>
2018-01-30 14:53:21 -0500

    easier reweighting

--- R/redist.R
+++ R/redist.R
@@ -879,19 +879,12 @@ redist.mcmc <- function(adjobj, popvec, nsims, ndists = NULL, initcds = NULL,
 #' population parity, geographic compactness, or other constraints are
 #' implemented.
 #'
-#' @usage redist.ipw(algout,
-#' resampleconstraint = c("pop", "compact", "segregation", "similar"),
-#' targetbeta, targetpop = NULL, temper = 0)
+#' @usage redist.ipw(algout, targetpop = NULL)
 #'
 #' @param algout An object of class "redist".
-#' @param resampleconstraint The constraint implemented in the simulations: one
-#' of "pop", "compact", "segregation", or "similar".
-#' @param targetbeta The target value of the constraint.
 #' @param targetpop The desired level of population parity. \code{targetpop} =
 #' 0.01 means that the desired distance from population parity is 1\%. The
 #' default is \code{NULL}.
-#' @param temper A flag for whether simulated tempering was used to improve the
-#' mixing of the Markov Chain. The default is \code{1}.
 #'
 #' @details This function allows users to resample redistricting plans using
 #' inverse probability weighting techniques described in Rubin (1987). This
@@ -961,16 +954,11 @@ redist.mcmc <- function(adjobj, popvec, nsims, ndists = NULL, initcds = NULL,
 #'
 #' ## Resample using inverse probability weighting.
 #' ## Target distance from parity is 20%
-#' alg_253_20_st <- redist.ipw(alg_253_20_st,
-#' resampleconstraint = "pop",
-#' targetbeta = -5.4,
-#' targetpop = .2, temper = 1)
+#' alg_253_20_st <- redist.ipw(alg_253_20_st, targetpop = .2)
+#'
 #' }
 #' @export
-redist.ipw <- function(algout,
-                       targetpop = NULL,
-                       temper = 0)
-{
+redist.ipw <- function(algout, targetpop = NULL){
 
     ## Warnings:
     if(!inherits(algout, "redist")){
@@ -978,11 +966,7 @@ redist.ipw <- function(algout,
     }
 
     ## Get indices drawn under target beta if tempering
-    if(temper == 1){
-        indbeta <- which(algout$beta_sequence == 1)
-    }else{
-        indbeta <- 1:ncol(algout$partitions)
-    }
+    indbeta <- which(algout$beta_sequence == 1)
 
     ## Get indices of draws that meet target population
     if(!is.null(targetpop)){
