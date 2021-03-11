d70bffc6be94191ffbf9ddbbe35fac34148930b9
Christopher Kenny <ctk44@cornell.edu>
2021-03-11 11:04:48 -0500

    restore enumpart + remove contexts

--- R/map.R
+++ R/map.R
@@ -32,136 +32,148 @@
 #' @examples
 #' \dontrun{
 #' library(redist)
-#' data("fl25")
-#' data("algdat.p10")
-#' cds <- algdat.p10$cdmat[,100]
+#' data('fl25')
+#' data('algdat.p10')
+#' cds <- algdat.p10$cdmat[, 100]
 #' redist.map(shp = fl25, plan = cds)
 #' }
 #'
 #' @concept plot
 #' @export
-redist.map <- function(shp = NULL, adj = NULL,  plan = NULL, centroids = TRUE,
-                       edges = TRUE, boundaries = TRUE,  drop = FALSE,
-                       title = "", adjacency, district_membership) {
-    if (!missing(adjacency)) {
-        .Deprecated(new = 'adj', old = 'adjacency')
-        adj <- adjacency
+redist.map <- function(shp = NULL, adj = NULL, plan = NULL, centroids = TRUE,
+                       edges = TRUE, boundaries = TRUE, drop = FALSE,
+                       title = '', adjacency, district_membership) {
+  if (!missing(adjacency)) {
+    .Deprecated(new = 'adj', old = 'adjacency')
+    adj <- adjacency
+  }
+  if (!missing(district_membership)) {
+    .Deprecated(new = 'plan', old = 'district_membership')
+    plan <- district_membership
+  }
+
+  # Check inputs
+  if (is.null(shp)) {
+    stop('Please provide an argument to "shp".')
+  }
+
+  if ('SpatialPolygonsDataFrame' %in% class(shp)) {
+    shp <- shp %>% st_as_sf()
+  } else if (!('sf' %in% class(shp))) {
+    stop('Please provide "shp" as a SpatialPolygonsDataFrame or sf object.')
+  }
+
+  if (!is.null(plan)) {
+    if (!any(class(plan) %in% c('numeric', 'integer', 'character'))) {
+      stop('Please provide "plan" as a vector.')
     }
-    if (!missing(district_membership)) {
-        .Deprecated(new = 'plan', old = 'district_membership')
-        plan <- district_membership
+    if (nrow(shp) != length(plan)) {
+      stop('Arguments "plan" and "shp" do not have same number of precincts.')
     }
+  }
 
-    # Check inputs
-    if (is.null(shp)) {
-        stop('Please provide an argument to "shp".')
-    }
+  if (!edges & drop) {
+    warning('edges FALSE while drop TRUE, assumed edges should be TRUE.')
+    edges <- TRUE
+  }
 
-    if ('SpatialPolygonsDataFrame' %in% class(shp)) {
-        shp <- shp %>%  st_as_sf()
-    } else if (!('sf' %in% class(shp))) {
-        stop('Please provide "shp" as a SpatialPolygonsDataFrame or sf object.')
-    }
+  if (drop & is.null(plan)) {
+    stop('drop is TRUE but no districts supplied')
+  }
 
-    if (!is.null(plan)) {
-        if (!any(class(plan) %in% c('numeric', 'integer', 'character'))) {
-            stop('Please provide "plan" as a vector.')
-        }
-        if (nrow(shp) != length(plan)) {
-            stop('Arguments "plan" and "shp" do not have same number of precincts.')
-        }
-    }
+  # Extract Centers
+  if (edges | centroids) {
+    suppressWarnings(centers <- st_centroid(shp))
+    st_crs(centers) <- st_crs(shp)
+  }
 
-    if (!edges & drop) {
-        warning('edges FALSE while drop TRUE, assumed edges should be TRUE.')
-        edges <- TRUE
-    }
-
-    if (drop & is.null(plan)) {
-        stop('drop is TRUE but no districts supplied')
-    }
-
-    # Extract Centers
-    if (edges | centroids) {
-        suppressWarnings(centers <- st_centroid(shp))
-        st_crs(centers) <- st_crs(shp)
-    }
-
-    # Extract Edges
-    if (edges) {
-        if (!is.null(adj)) {
-            nb <- lapply(adj, function(x) { x + 1L })
-        } else{
-            adj <- redist.adjacency(shp)
-            nb <- lapply(adj, function(x) { x + 1L })
-        }
-
-        edgedf <- tibble(start = rep(1:length(nb), lengths(nb)),
-                   finish = unlist(nb))
-        edgedf <- edgedf %>%
-            rowwise() %>%
-            mutate(i = min(start, finish), j = max(start, finish)) %>%
-            select(i, j)
-        edgedf <- edgedf[!duplicated(edgedf), ]
-
-        edgedf <- edgedf %>% rowwise() %>%
-            mutate(geometry = st_sfc(st_linestring(matrix(
-                c(as.numeric(centers$geometry[[i]]),
-                  as.numeric(centers$geometry[[j]])),
-                nrow = 2,
-                byrow = TRUE
-            ))))
-
-        suppressWarnings(nb <- sf::st_as_sf(edgedf))
-        st_crs(nb) <- st_crs(shp)
-    }
-
-
-    # Drop Edges that cross District Boundaries
-    if (drop & !is.null(plan)) {
-        nb <- nb %>%
-            filter(plan[i] == plan[j])
-    }
-
-    # Create Plot
-    if (!is.null(plan)) {
-        plan <- as.factor(plan)
-
-        if (!is.null(adj)) {
-            plan = as.factor(color_graph(adj, as.integer(plan)))
-        }
-
-        plot <- ggplot(shp) +
-            geom_sf(aes(fill = plan),
-                    size = 0.3 * boundaries,
-                    color = "#444444") +
-            theme_minimal()     +
-            labs(fill = 'District Membership', x = 'Longitude',
-                y = 'Latitude', title = title) +
-            theme(legend.position = "bottom")
-
-        if (inherits(shp, "redist_map")) {
-            PAL = c("#6D9537", "#364B7F", "#DCAD35", "#9A9BB9", "#2A4E45", "#7F4E28")
-            plot = plot + ggplot2::guides(fill = FALSE) +
-                ggplot2::scale_fill_manual(values = PAL)
-        }
+  # Extract Edges
+  if (edges) {
+    if (!is.null(adj)) {
+      nb <- lapply(adj, function(x) {
+        x + 1L
+      })
     } else {
-        plot <- ggplot(shp) +
-            geom_sf() +
-            theme_minimal() +
-            labs(x = 'Longitude', y = 'Latitude', title = title)
+      adj <- redist.adjacency(shp)
+      nb <- lapply(adj, function(x) {
+        x + 1L
+      })
     }
 
-    if (centroids) {
-        plot <- plot + geom_sf(data = centers)
+    edgedf <- tibble(
+      start = rep(1:length(nb), lengths(nb)),
+      finish = unlist(nb)
+    )
+    edgedf <- edgedf %>%
+      rowwise() %>%
+      mutate(i = min(start, finish), j = max(start, finish)) %>%
+      select(i, j)
+    edgedf <- edgedf[!duplicated(edgedf), ]
+
+    edgedf <- edgedf %>%
+      rowwise() %>%
+      mutate(geometry = st_sfc(st_linestring(matrix(
+        c(
+          as.numeric(centers$geometry[[i]]),
+          as.numeric(centers$geometry[[j]])
+        ),
+        nrow = 2,
+        byrow = TRUE
+      ))))
+
+    suppressWarnings(nb <- sf::st_as_sf(edgedf))
+    st_crs(nb) <- st_crs(shp)
+  }
+
+
+  # Drop Edges that cross District Boundaries
+  if (drop & !is.null(plan)) {
+    nb <- nb %>%
+      filter(plan[i] == plan[j])
+  }
+
+  # Create Plot
+  if (!is.null(plan)) {
+    plan <- as.factor(plan)
+
+    if (!is.null(adj)) {
+      plan <- as.factor(color_graph(adj, as.integer(plan)))
     }
 
-    if (edges) {
-        plot <- plot + geom_sf(data = nb)
-    }
+    plot <- ggplot(shp) +
+      geom_sf(aes(fill = plan),
+        size = 0.3 * boundaries,
+        color = '#444444'
+      ) +
+      theme_minimal() +
+      labs(
+        fill = 'District Membership', x = 'Longitude',
+        y = 'Latitude', title = title
+      ) +
+      theme(legend.position = 'bottom')
 
-    # return plot
-    return(plot)
+    if (inherits(shp, 'redist_map')) {
+      PAL <- c('#6D9537', '#364B7F', '#DCAD35', '#9A9BB9', '#2A4E45', '#7F4E28')
+      plot <- plot + ggplot2::guides(fill = FALSE) +
+        ggplot2::scale_fill_manual(values = PAL)
+    }
+  } else {
+    plot <- ggplot(shp) +
+      geom_sf() +
+      theme_minimal() +
+      labs(x = 'Longitude', y = 'Latitude', title = title)
+  }
+
+  if (centroids) {
+    plot <- plot + geom_sf(data = centers)
+  }
+
+  if (edges) {
+    plot <- plot + geom_sf(data = nb)
+  }
+
+  # return plot
+  return(plot)
 }
 
 
@@ -185,51 +197,51 @@ redist.map <- function(shp = NULL, adj = NULL,  plan = NULL, centroids = TRUE,
 #'
 #' @examples
 #' \dontrun{
-#' data("fl25")
-#' redist.choropleth(shp = fl25, fill = BlackPop/TotPop)
-#' DVS <- fl25$obama/(fl25$mccain+fl25$obama)
+#' data('fl25')
+#' redist.choropleth(shp = fl25, fill = BlackPop / TotPop)
+#' DVS <- fl25$obama / (fl25$mccain + fl25$obama)
 #' redist.choropleth(shp = fl25, fill = DVS, limit_colors = c('red', 'blue'))
 #' }
 #'
 #' @concept plot
 #' @export
-redist.choropleth <- function(shp, fill = NULL, fill_label = "", title = "",
+redist.choropleth <- function(shp, fill = NULL, fill_label = '', title = '',
                               grad = 1, lwd = 0) {
 
-    # Check inputs
-    if(missing(shp)) {
-        stop('Please provide an argument to "shp".')
-    }
-    if('SpatialPolygonsDataFrame' %in% class(shp)) {
-        shp <- shp %>%  st_as_sf()
-    } else if(!('sf' %in% class(shp))) {
-        stop('Please provide "shp" as a SpatialPolygonsDataFrame or sf object.')
-    }
-    
-    fill <- rlang::eval_tidy(rlang::enquo(fill), shp)
-    if (!is.null(fill) && !is.numeric(fill) && inherits(shp, "redist_map")) {
-        fill = as.factor(color_graph(get_adj(shp), as.integer(as.factor(fill))))
-    }
+  # Check inputs
+  if (missing(shp)) {
+    stop('Please provide an argument to "shp".')
+  }
+  if ('SpatialPolygonsDataFrame' %in% class(shp)) {
+    shp <- shp %>% st_as_sf()
+  } else if (!('sf' %in% class(shp))) {
+    stop('Please provide "shp" as a SpatialPolygonsDataFrame or sf object.')
+  }
 
-    plot <- ggplot(shp, aes(fill = fill)) +
-        geom_sf(size = 0, color = NA) +
-        theme_void() +
-        labs(fill = fill_label, x = 'Longitude', y = 'Latitude', title = title) +
-        theme(legend.position = "bottom")
+  fill <- rlang::eval_tidy(rlang::enquo(fill), shp)
+  if (!is.null(fill) && !is.numeric(fill) && inherits(shp, 'redist_map')) {
+    fill <- as.factor(color_graph(get_adj(shp), as.integer(as.factor(fill))))
+  }
 
-    if (!is.null(fill)) {
-        if (is.numeric(fill)) {
-            if (min(fill, na.rm=T) >= 0 & max(fill, na.rm=T) <= 1) {
-                plot <- plot + lims(fill=c(0, 1))
-            }
-        } else {
-            PAL = c("#6D9537", "#364B7F", "#DCAD35", "#9A9BB9", "#2A4E45", "#7F4E28")
-            plot <- plot + ggplot2::scale_fill_manual(values=PAL) +
-               ggplot2::guides(fill=FALSE)
-        }
+  plot <- ggplot(shp, aes(fill = fill)) +
+    geom_sf(size = 0, color = NA) +
+    theme_void() +
+    labs(fill = fill_label, x = 'Longitude', y = 'Latitude', title = title) +
+    theme(legend.position = 'bottom')
+
+  if (!is.null(fill)) {
+    if (is.numeric(fill)) {
+      if (min(fill, na.rm = T) >= 0 & max(fill, na.rm = T) <= 1) {
+        plot <- plot + lims(fill = c(0, 1))
+      }
+    } else {
+      PAL <- c('#6D9537', '#364B7F', '#DCAD35', '#9A9BB9', '#2A4E45', '#7F4E28')
+      plot <- plot + ggplot2::scale_fill_manual(values = PAL) +
+        ggplot2::guides(fill = FALSE)
     }
+  }
 
-    return(plot)
+  return(plot)
 }
 
 globalVariables(c('start', 'finish'))
--- tests/testthat/test_adj.R
+++ tests/testthat/test_adj.R
@@ -1,5 +1,3 @@
-context("Adjacency Lists")
-
 test_that("Adjacency lists are reduced correctly", {
     g = list(c(1L, 2L), c(0L, 2L), c(0L, 1L, 3L), 2L)
     g_exp = list(c(1L, 2L), 0L, 0L)
