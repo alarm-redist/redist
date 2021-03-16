a663bfa3bf3f386a05ffc0bbcf485553d05fc6c4
Christopher Kenny <ctk44@cornell.edu>
2021-03-15 22:09:21 -0400

    docs - comp & county split constraint
    + up site

--- tests/testthat/test-mcmc.R
+++ tests/testthat/test-mcmc.R
@@ -11,3 +11,22 @@ test_that("mcmc works", {
   expect_true(all(par <= 0.1))
 
 })
+
+test_that("mcmc countysplit works", {
+  set.seed(1, kind = "Mersenne-Twister", normal.kind = "Inversion")
+  
+  
+  cty <- rep(1, 25)
+  cty[1:4] <- 2
+  
+  nsims <- 10
+  out <- redist.mcmc(adj = adj, total_pop = pop, init_plan = algdat.p10$cdmat[,1],
+                     nsims = nsims, ndists = 3, verbose = FALSE, pop_tol = 0.1,
+                     counties = cty, constraint = 'countysplit', constraintweights = 5)
+  par <- redist.parity(out$plans, total_pop = pop)
+  
+  expect_equal(out$nsims, nsims)
+  expect_equal(range(out$plans), c(0, 2))
+  expect_true(all(par <= 0.1))
+  
+})
