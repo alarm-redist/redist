7eb97ed79320e7e6391684288815e51e848e036a
Cory McCartan <cory.mccartan@gmail.com>
2021-03-20 12:23:05 -0400

    update freeze

--- R/freeze.R
+++ R/freeze.R
@@ -2,9 +2,11 @@
 #'
 #'
 #' @param adj Required, zero indexed adjacency list.
-#' @param plan Recommended, whether to create separate groups by district.
-#' @param freeze_row Required, logical vector where TRUE freezes and FALSE lets a precinct
-#' stay free or a vector of indices to freeze
+#' @param freeze_row Required, logical vector where TRUE freezes and FALSE lets
+#'   a precinct stay free or a vector of indices to freeze
+#' @param plan A vector of district assignments, which if provided will create
+#'   separate groups by district. Recommended. In \code{freeze} defaults to the
+#'   existing plan, if one exists.
 #'
 #' @return integer vector to group by
 #' @export
@@ -14,9 +16,9 @@
 #' data(fl25_enum)
 #' data(fl25_adj)
 #' plan = fl25_enum$plans[, 5118]
-#' freeze_id <- redist.freeze(adj = fl25_adj, plan = plan,
-#'                            freeze_row = (plan == 2))
-redist.freeze <- function(adj, plan, freeze_row){
+#' freeze_id <- redist.freeze(adj = fl25_adj, freeze_row = (plan == 2),
+#'                            plan = plan)
+redist.freeze <- function(adj, freeze_row, plan=rep(1, length(adj))){
   if(missing(adj)){
     stop('Please provide an object to adj.')
   }
@@ -41,65 +43,43 @@ redist.freeze <- function(adj, plan, freeze_row){
     stop('freeze_row must be a logical vector or have numeric/integer indices.')
   }
 
-  if(!missing(plan)){
-    tb <- tibble(plan = plan, freeze_row = freeze_row) %>%
+  tb <- tibble(plan = plan, freeze_row = freeze_row) %>%
       group_by(plan, freeze_row) %>%
       mutate(id = cur_group_id()) %>%
       ungroup()
-    cont <- contiguity(adj = adj, group = tb$id)
+  cont <- contiguity(adj = adj, group = tb$id)
   tb <- tb %>% mutate(cont = cont) %>%
-    mutate(rn = row_number()) %>%
-    group_by(plan, cont, freeze_row) %>%
-    mutate(rn = ifelse(freeze_row, min(rn), rn)) %>%
-    ungroup() %>%
-    group_by(rn) %>%
-    mutate(gid = cur_group_id())
-
-
-  # then do the same thing that cores does
-  } else {
-    cont <- contiguity(adj = adj, group = as.integer(freeze_row))
-    tb <- tibble(freeze_row = freeze_row, cont = cont) %>%
       mutate(rn = row_number()) %>%
-      group_by(freeze_row, cont) %>%
+      group_by(plan, cont, freeze_row) %>%
       mutate(rn = ifelse(freeze_row, min(rn), rn)) %>%
       ungroup() %>%
       group_by(rn) %>%
       mutate(gid = cur_group_id())
-  }
 
   return(tb$gid)
 }
 
 #' @rdname redist.freeze
 #'
-#' @param adj Required, zero indexed adjacency list.
-#' @param plan Recommended, whether to create separate groups by district.
-#' @param freeze_row Required, logical vector where TRUE freezes and FALSE lets a precinct
-#' stay free or a vector of indices to freeze
 #' @param .data a \code{\link{redist_map}} object
 #'
 #' @concept prepare
 #' @export
-freeze <- function(freeze_row,
-                   adj, plan, .data=get0(".", parent.frame())){
-  if (!inherits(.data, "redist_map")){
-    stop("Must provide `.data` if not called within a pipe")
-  }
-  if(missing(freeze_row)){
-    stop('Please provide an argument to freeze_row.')
-  }
-  if(missing(adj)){
-   adj <- get_adj(.data)
-  }
-  if(missing(plan)){
-   plan <- as.integer(get_existing(.data))
-  }
+freeze <- function(freeze_row, plan, .data=get0(".", parent.frame())) {
+    if (!inherits(.data, "redist_map"))
+        stop("Must provide `.data` if not called within a pipe")
+    if (missing(freeze_row))
+        stop('Please provide an argument to freeze_row.')
 
-  freeze_row <- rlang::eval_tidy(rlang::enquo(freeze_row), .data)
+    adj <- get_adj(.data)
+    if (missing(plan))
+        plan <- as.integer(get_existing(.data))
+    if (isFALSE(plan) || is.null(plan))
+        plan <- rep(1, length(adj))
 
-  return(redist.freeze(adj = adj, plan = plan, freeze_row = freeze_row))
+    freeze_row <- rlang::eval_tidy(rlang::enquo(freeze_row), .data)
 
+    redist.freeze(adj = adj, freeze_row = freeze_row, plan = plan)
 }
 
 globalVariables('rn')
