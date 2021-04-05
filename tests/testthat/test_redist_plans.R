e43860ab6f35b82f3bf0c4adf54a5720d2e56a4b
Cory McCartan <cory.mccartan@gmail.com>
2021-04-05 18:40:36 -0400

    finish basic redist_plans tests; fix plotting bugs

--- tests/testthat/test_redist_plans.R
+++ tests/testthat/test_redist_plans.R
@@ -5,10 +5,41 @@ test_that("constructor works", {
     expect_s3_class(x, "tbl")
     expect_true(is.matrix(as.matrix(x)))
     expect_equal(attr(x, "prec_pop"), fl25$pop)
-    expect_output(print(x), "drawn using Enumpart")
-    expect_output(print(x), "927 sampled plans with 3 districts")
+    expect_null(weights(x))
+    expect_output(print(x), "drawn using Enumpart", fixed=T)
+    expect_output(print(x), "927 sampled plans with 3 districts", fixed=T)
 })
 
+test_that("add_reference works", {
+    fl = redist_map(fl25, ndists=3, pop_tol=0.1)
+    x = redist_plans(plans_10, fl, "enumpart") %>%
+        add_reference(plans_10[,1])
+    expect_s3_class(x, "redist_plans")
+    expect_equal(as.character(head(x$draw, 3)), rep("plans_10[, 1]", 3))
+    expect_equal(colnames(as.matrix(x))[1], "plans_10[, 1]")
+    expect_equal(ncol(as.matrix(x)), ncol(plans_10)+1)
+    expect_equal(nrow(as.matrix(x)), nrow(plans_10))
+})
+
+test_that("subsetting works", {
+    fl = redist_map(fl25, ndists=3, pop_tol=0.1)
+    x = redist_plans(plans_10, fl, "enumpart") %>%
+        add_reference(plans_10[,1])
+    x_samp = subset_sampled(x)
+    x_ref = subset_ref(x)
+    expect_equal(ncol(as.matrix(x_samp)), ncol(plans_10))
+    expect_equal(ncol(as.matrix(x_ref)), 1)
+})
+
+test_that("plotting works", {
+    fl = redist_map(fl25, ndists=3, pop_tol=0.1)
+    x = redist_plans(plans_10, fl, "enumpart") %>%
+        add_reference(plans_10[,1])
+    out = plot(x)
+    expect_s3_class(out, "ggplot")
+    out = hist(x, total_pop)
+    expect_s3_class(out, "ggplot")
+})
 
 test_that("get_mh_acceptance_rate works", {
   iowa_map <- redist_map(iowa, pop_tol = 0.01, existing_plan = cd_2010)
