5d99bd4e8fa1b01ee07a553b41dc3756d5cbecef
Cory McCartan <cory.mccartan@gmail.com>
2021-04-05 21:22:59 -0400

    try to stop hanging tests

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -1,33 +1,27 @@
 test_that("redist.plot.map works", {
   out <- redist.plot.map(shp = iowa, plan = iowa$cd_2010)
-
   expect_true('ggplot' %in% class(out))
 
-  out <- iowa %>% redist_map(existing_plan = cd_2010) %>% redist.plot.map(shp = ., plan = get_existing(.))
+  iowa_map = redist_map(iowa,existing_plan = cd_2010)
 
+  out <- iowa_map  %>% redist.plot.map(shp = ., plan = get_existing(.))
   expect_true('ggplot' %in% class(out))
 
-  out <- iowa %>% redist_map(existing_plan = cd_2010) %>% redist.plot.map(shp = ., plan = cd_2010)
-
+  out <- iowa_map %>% redist.plot.map(shp = ., plan = cd_2010)
   expect_true('ggplot' %in% class(out))
 
-  out <- iowa %>% redist_map(existing_plan = cd_2010) %>% redist.plot.map(shp = ., plan = cd_2010, fill = white)
-
+  out <- iowa_map %>% redist.plot.map(shp = ., plan = cd_2010, fill = white)
   expect_true('ggplot' %in% class(out))
 })
 
 
 test_that("redist.plot.adj works", {
   out <- redist.plot.adj(shp = iowa, plan = iowa$cd_2010)
-
   expect_true('ggplot' %in% class(out))
 
-  out <- iowa %>% redist_map(existing_plan = cd_2010) %>% redist.plot.adj(shp = ., plan = get_existing(.))
-
+  out <- iowa_map %>% redist.plot.adj(shp = ., plan = get_existing(.))
   expect_true('ggplot' %in% class(out))
 
-  out <- iowa %>% redist_map(existing_plan = cd_2010) %>% redist.plot.map(shp = ., plan = cd_2010)
-
+  out <- iowa_map %>% redist.plot.map(shp = ., plan = cd_2010)
   expect_true('ggplot' %in% class(out))
-
 })
