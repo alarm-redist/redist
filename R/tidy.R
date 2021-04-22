57a4d6f7ef84268a1e95345f006a23d25f3c83ce
Cory McCartan <cory.mccartan@gmail.com>
2021-04-22 14:39:18 -0400

    fix cur_map() inside merge_by

--- R/tidy.R
+++ R/tidy.R
@@ -28,11 +28,12 @@ get_cur_df = function(dplyr_funcs) {
 
 #' Helper function to get current map object
 #' @noRd
-cur_map = function(verbs=c("mutate", "summarize", "filter",
-                           "arrange", "transmute")) {
+cur_map = function(verbs=c("mutate", "summarize", "merge_by",
+                           "filter", "arrange", "transmute")) {
     get_cur_df(list(mutate=mutate.redist_map,
                     transmute=transmute.redist_map,
                     summarize=summarise.redist_map,
+                    merge_by=merge_by,
                     filter=filter.redist_map,
                     arrange=arrange.redist_map)[verbs])
 }
