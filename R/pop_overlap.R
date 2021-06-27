2a853af4bd2416a2186c80ba775025df4673193a
Christopher Kenny <ctk44@cornell.edu>
2021-06-27 11:57:28 -0400

    flexible weighting

--- R/pop_overlap.R
+++ R/pop_overlap.R
@@ -5,7 +5,7 @@
 #' This implementation assumes none of the precincts in plan_old or plan_new are
 #' split.
 #'
-#' @param plan_old The reference plan to compare against
+#' @param plan_old The reference or original plan to compare against
 #' @param plan_new The new plan to compare to the reference plan
 #' @param total_pop The total population by precinct This can also take a redist_map
 #' object and will use the population in that object. If nothing is provided, it weights
@@ -84,9 +84,15 @@ redist.dist.pop.overlap <- function(plan_old, plan_new, total_pop, normalize_row
 #' @param total_pop The total population by precinct This can also take a redist_map
 #' object and will use the population in that object. If nothing is provided, it weights
 #' all entries in plan equally.
+#' @param weighting Should weighting be done by sum of populations `'s'`, mean of 
+#' populations `'m'`, geometric mean of populations `'g'`, or none `'n'`
 #' @param normalize Should entries be normalized by the total population
+#' @param index_only Default is FALSE. TRUE returns only one numeric index, the 
+#' mean of the upper triangle of the matrix, under the weighting and normalization  
+#' chosen.
 #' @param return_mat Defaults to FALSE, where it returns the summary by row. If
-#' TRUE returns matrix with length(plan_old) rows and columns
+#' TRUE returns matrix with length(plan_old) rows and columns. Ignored if 
+#' index_only = TRUE.
 #'
 #' @return numeric vector with length(plan_old) entries
 #' @export
@@ -95,10 +101,16 @@ redist.dist.pop.overlap <- function(plan_old, plan_new, total_pop, normalize_row
 #' set.seed(5)
 #' data(iowa)
 #' iowa_map <- redist_map(iowa, total_pop = pop, pop_tol = 0.01, ndists = 4)
-#' plans <- redist_smc(iowa_map, 2)
+#' plans <- redist_smc(iowa_map, 2, silent = TRUE)
 #' plans_mat <- get_plans_matrix(plans)
-#' ov <- redist.prec.pop.overlap(plans_mat[, 1], plans_mat[, 2], iowa_map)
-redist.prec.pop.overlap <- function(plan_old, plan_new, total_pop, normalize = TRUE, return_mat = FALSE) {
+#' ov_vec <- redist.prec.pop.overlap(plans_mat[, 1], plans_mat[, 2], iowa_map)
+#' redist.prec.pop.overlap(plans_mat[, 1], plans_mat[, 2], iowa_map,  weighting = 's', 
+#' normalize = FALSE, index_only = TRUE)
+redist.prec.pop.overlap <- function(plan_old, plan_new, total_pop, weighting = 's', 
+                                    normalize = TRUE, index_only = FALSE, return_mat = FALSE) {
+  
+  weighting <- match.arg(weighting, choices = c('s','m','g','n'))
+  
   if (missing(plan_old)) {
     stop('Please pass an argument to `plan_old`.')
   } else {
@@ -114,26 +126,45 @@ redist.prec.pop.overlap <- function(plan_old, plan_new, total_pop, normalize = T
     stop('plan_old and plan_new must have the same number of entries.')
   }
 
+  nprec <- length(plan_old)
+  
   if (missing(total_pop)) {
-    total_pop <- rep(1L, length(plan_old))
+    total_pop <- rep(1L, nprec)
   } else {
     if (inherits(total_pop, 'redist_map')) {
       total_pop <- total_pop[[attr(total_pop, 'pop_col')]]
     }
   }
+  
   total_pop <- matrix(total_pop, nrow = length(total_pop))
-  wts <- sqrt(total_pop %*% t(total_pop))
+  
+  if (weighting == 's') { # weight by sum of ith and jth pop
+    cmat <- matrix(rep(total_pop, nprec), ncol = nprec)
+    rmat <- t(cmat)
+    wts <- (cmat + rmat)
+  } else if (weighting == 'g') { # weight by geometric mean of ith and jth pop
+    wts <- sqrt(total_pop %*% t(total_pop))
+  } else if (weighting == 'm') {# weight by mean of ith and jth pop
+    cmat <- matrix(rep(total_pop, nprec), ncol = nprec)
+    rmat <- t(cmat)
+    wts <- (cmat + rmat)/2
+  } else { #if (weighting == 'n') { # no weights (matrix of 1s)
+    wts <- matrix(1, nprec, nprec)
+  }
 
   mat_old <- prec_cooccur(plan_old, 1)
   mat_new <- prec_cooccur(plan_new, 1)
 
-  wted_mat <- c(total_pop) * matrix(as.numeric((mat_old - mat_new) != 0), 
-                                    nrow = length(total_pop), ncol = length(total_pop))
+  wted_mat <- wts * abs(mat_old - mat_new)
 
   if (normalize) {
     wted_mat <- wted_mat / sum(total_pop)
   }
-
+  
+  if (index_only) {
+    return(mean(wted_mat[upper.tri(wted_mat)]))
+  }
+  
   if (return_mat) {
     return(wted_mat)
   }
