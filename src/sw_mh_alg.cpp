68310361a648ed4365158581c2f536849f680e84
Christopher Kenny <ctk44@cornell.edu>
2022-01-27 15:27:01 -0500

    Update sw_mh_alg.cpp

--- src/sw_mh_alg.cpp
+++ src/sw_mh_alg.cpp
@@ -194,7 +194,7 @@ List swMH(List aList,
     NumericVector psi_upd;
     CharacterVector psi_names = CharacterVector::create(
         "pop_dev", "splits", "multisplits",
-        "segregation", "grp_pow", "grp_hinge",
+        "segregation", "grp_pow", "grp_hinge", "grp_inv_hinge",
         "compet", "status_quo", "incumbency",
         "polsby", "fry_hold", "log_st", "edges_removed",
         "qps", "custom"
