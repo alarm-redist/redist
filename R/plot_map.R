1beb754adf039d8d170b71f4d938c2ed18008dbc
Cory McCartan <cory.mccartan@gmail.com>
2022-04-09 19:38:20 -0400

    map improvements

--- R/plot_map.R
+++ R/plot_map.R
@@ -43,150 +43,135 @@
 #' @export
 redist.plot.map <- function(shp, adj, plan = NULL, fill = NULL, fill_label = '',
                             zoom_to=NULL, boundaries=is.null(fill), title='') {
-  # Check inputs
-  if (missing(shp)) {
-    stop('Please provide an argument to "shp".')
-  }
-
-  if ('SpatialPolygonsDataFrame' %in% class(shp)) {
-    shp <- shp %>% st_as_sf()
-  } else if (!('sf' %in% class(shp))) {
-    stop('Please provide "shp" as a SpatialPolygonsDataFrame or sf object.')
-  }
-
-  plan <- eval_tidy(enquo(plan), shp)
-  if (!is.null(plan)) {
-    if (!any(class(plan) %in% c('numeric', 'integer', 'character'))) {
-      stop('Please provide "plan" as a vector.')
+    # Check inputs
+    if ('SpatialPolygonsDataFrame' %in% class(shp)) {
+        shp <- shp %>% st_as_sf()
+    } else if (!inherits(shp, "sf")) {
+        cli_abort('{.arg shp} must be a {.cls SpatialPolygonsDataFrame} or {.cls sf} object.')
     }
-    if (nrow(shp) != length(plan)) {
-      stop('Arguments "plan" and "shp" do not have same number of precincts.')
+
+    plan <- eval_tidy(enquo(plan), shp)
+    if (!is.null(plan)) {
+        if (!is.numeric(plan)) {
+            cli_abort("{.arg} plan must be a numeric vector.")
+        }
+        if (nrow(shp) != length(plan)) {
+            cli_abort('{.arg plan} and {.arg shp} must have same number of precincts.')
+        }
     }
-  }
-  fill <- eval_tidy(enquo(fill), shp)
+    fill <- eval_tidy(enquo(fill), shp)
 
-  # Create Plot
-  if (!is.null(plan)) {
-    if (inherits(shp, 'redist_map') & is.null(fill)) {
-      plan <- as.factor(plan)
-      adj <- get_adj(shp)
-      ndists = length(unique(plan))
-      if (ndists > 6)
-          plan <- as.factor(color_graph(adj, as.integer(plan)))
-
-
-      plot <- ggplot(shp) +
-        geom_sf(aes(fill = plan), lwd = 0.3 * boundaries,
-                color = if (boundaries) "#444444" else NA) +
-        theme_void() +
-        labs(fill = 'District', title = title) +
-        theme(legend.position = 'bottom')
+    # Create Plot
+    if (!is.null(plan)) {
+        if (inherits(shp, 'redist_map') & is.null(fill)) {
+            plan <- as.factor(plan)
+            adj <- get_adj(shp)
+            ndists = length(unique(plan))
+            if (ndists > 6)
+                plan <- as.factor(color_graph(adj, as.integer(plan)))
 
 
-      PAL <- c('#6D9537', '#364B7F', '#DCAD35', '#9A9BB9', '#2A4E45', '#7F4E28')
-      if (ndists > 6) {
-          plot <- plot + ggplot2::guides(fill = 'none') +
-            ggplot2::scale_fill_manual(values = PAL)
-      } else {
-          plot <- plot + ggplot2::scale_fill_manual(values = PAL)
-      }
-    } else if(inherits(shp, 'redist_map')) {
-      plan <- as.factor(plan)
-      adj <- get_adj(shp)
-      ndists = length(unique(plan))
-      if (ndists > 6)
-          plan <- as.factor(color_graph(adj, as.integer(plan)))
-
-      if(max(fill, na.rm = TRUE) > 1){
-        fill <- fill/max(fill)
-      }
-
-      plot <- ggplot(shp) +
-        geom_sf(aes(fill = plan, alpha = fill), lwd = 0.3 * boundaries,
-                color = if (boundaries) "#444444" else NA) +
-        theme_void() +
-        labs(alpha = fill_label, title = title) +
-        theme(legend.position = 'bottom')
+            plot <- ggplot(shp) +
+                geom_sf(aes(fill = plan), lwd = 0.3 * boundaries,
+                        color = if (boundaries) "#444444" else NA) +
+                theme_void() +
+                labs(fill = 'District', title = title) +
+                theme(legend.position = 'bottom')
 
 
-      PAL <- c('#6D9537', '#364B7F', '#DCAD35', '#9A9BB9', '#2A4E45', '#7F4E28')
-      plot <- plot + ggplot2::guides(fill = 'none') +
-        ggplot2::scale_fill_manual(values = PAL)  +
-        ggplot2::scale_alpha_continuous(range = c(0,1))
+            PAL <- c('#6D9537', '#364B7F', '#DCAD35', '#9A9BB9', '#2A4E45', '#7F4E28')
+            if (ndists > 6) {
+                plot <- plot + ggplot2::guides(fill = 'none') +
+                    ggplot2::scale_fill_manual(values = PAL)
+            } else {
+                plot <- plot + ggplot2::scale_fill_manual(values = PAL)
+            }
+        } else if (inherits(shp, 'redist_map')) {
+            plan <- as.factor(plan)
+            adj <- get_adj(shp)
+            ndists = length(unique(plan))
+            if (ndists > 6) {
+                plan <- as.factor(color_graph(adj, as.integer(plan)))
+            }
+
+            if (max(fill, na.rm = TRUE) > 1) {
+                fill <- fill/max(fill)
+            }
+
+            plot <- ggplot(shp) +
+                geom_sf(aes(fill = plan, alpha = fill), lwd = 0.3 * boundaries,
+                        color = if (boundaries) "#444444" else NA) +
+                theme_void() +
+                labs(alpha = fill_label, title = title) +
+                theme(legend.position = 'bottom')
+
+
+            PAL <- c('#6D9537', '#364B7F', '#DCAD35', '#9A9BB9', '#2A4E45', '#7F4E28')
+            plot <- plot + ggplot2::guides(fill = 'none') +
+                ggplot2::scale_fill_manual(values = PAL)  +
+                ggplot2::guides(alpha="none")
+        } else {
+            if(is.null(fill)) { # plan but no fill
+                plot <- ggplot(shp) +
+                    geom_sf(aes(fill = as.character(plan)), lwd = 0.3 * boundaries,
+                            color = if (boundaries) "#444444" else NA) +
+                    theme_void() +
+                    labs(fill = 'District Membership', title = title) +
+                    theme(legend.position = 'bottom')
+            } else { # plan and fill
+                if(max(fill, na.rm = TRUE) > 1){
+                    fill <- fill/max(fill)
+                }
+
+                plot <- ggplot(shp) +
+                    geom_sf(aes(fill = as.character(plan), alpha = fill), lwd = 0.3 * boundaries,
+                            color = if (boundaries) "#444444" else NA) +
+                    theme_void() +
+                    labs(fill = 'District Membership', alpha = fill_label, title = title) +
+                    ggplot2::guides(alpha="none") +
+                    theme(legend.position = 'bottom')
+            }
+
+        }
+    } else if (!is.null(fill)) { # no plan but fill
+        recolor <- FALSE
+        if (inherits(shp, "redist_map") && (is.character(fill) || is.factor(fill))) {
+            adj <- get_adj(shp)
+            nlevels <- length(unique(fill))
+            fill <- as.factor(fill)
+            recolor <- TRUE
+            if (nlevels > 6) {
+                fill <- as.factor(color_graph(adj, as.integer(fill)))
+            }
+        }
+
+        plot <- ggplot(shp) +
+            geom_sf(aes(fill = fill), lwd = 0.3 * boundaries,
+                    color = if (boundaries) "#444444" else NA) +
+            theme_void() +
+            labs(fill = fill_label, title = title) +
+            theme(legend.position = 'bottom')
+
+        if (recolor) {
+            PAL <- c('#6D9537', '#364B7F', '#DCAD35', '#9A9BB9', '#2A4E45', '#7F4E28')
+            plot <- plot + ggplot2::guides(fill = 'none') +
+                ggplot2::scale_fill_manual(values = PAL)
+        }
     } else {
-      if(is.null(fill)){ # plan but no fill
         plot <- ggplot(shp) +
-          geom_sf(aes(fill = as.character(plan)), lwd = 0.3 * boundaries,
-                  color = if (boundaries) "#444444" else NA) +
-          theme_void() +
-          labs(fill = 'District Membership', title = title) +
-          theme(legend.position = 'bottom')
-      } else { # plan and fill
-        if(max(fill, na.rm = TRUE) > 1){
-          fill <- fill/max(fill)
-        }
-
-        plot <- ggplot(shp) +
-          geom_sf(aes(fill = as.character(plan), alpha = fill), lwd = 0.3 * boundaries,
-                  color = if (boundaries) "#444444" else NA) +
-          theme_void() +
-          labs(fill = 'District Membership', alpha = fill_label, title = title) +
-          theme(legend.position = 'bottom')
-
-        if (min(fill, na.rm = TRUE) >= 0 & max(fill, na.rm = TRUE) <= 1) {
-          plot <- plot + lims(alpha = c(0, 1))
-        }
-
-      }
-
-    }
-  } else if (!is.null(fill)) { # no plan but fill
-    recolor <- FALSE
-    if (inherits(shp, "redist_map") && (is.character(fill) || is.factor(fill))) {
-      adj <- get_adj(shp)
-      nlevels <- length(unique(fill))
-      fill <- as.factor(fill)
-      recolor <- TRUE
-      if (nlevels > 6) {
-        fill <- as.factor(color_graph(adj, as.integer(fill)))
-      }
+            geom_sf(color = if (boundaries) "#444444" else NA) +
+            theme_void()
     }
 
-    plot <- ggplot(shp) +
-      geom_sf(aes(fill = fill), lwd = 0.3 * boundaries,
-              color = if (boundaries) "#444444" else NA) +
-      theme_void() +
-      labs(fill = fill_label, title = title) +
-      theme(legend.position = 'bottom')
-
-    if (recolor) {
-      PAL <- c('#6D9537', '#364B7F', '#DCAD35', '#9A9BB9', '#2A4E45', '#7F4E28')
-      plot <- plot + ggplot2::guides(fill = 'none') +
-        ggplot2::scale_fill_manual(values = PAL)
+    zoom_to = eval_tidy(enquo(zoom_to), shp)
+    if (!is.null(zoom_to)) {
+        bbox = sf::st_bbox(sf::st_geometry(shp)[zoom_to])
+        plot <- plot + ggplot2::coord_sf(xlim=c(bbox$xmin, bbox$xmax),
+                                         ylim=c(bbox$ymin, bbox$ymax))
     }
 
-    if(is.numeric(fill) && min(fill, na.rm = TRUE) >= 0 &&
-           max(fill, na.rm = TRUE) <= 1){
-        plot <- plot + ggplot2::scale_fill_gradient(low = '#ffffff',
-                                                    high = '#08306b',
-                                                    limits = c(0, 1))
-    }
 
-  } else   {
-    plot <- ggplot(shp) +
-      geom_sf(color = if (boundaries) "#444444" else NA) +
-      theme_void()
-  }
-
-  zoom_to = eval_tidy(enquo(zoom_to), shp)
-  if (!is.null(zoom_to)) {
-      bbox = sf::st_bbox(sf::st_geometry(shp)[zoom_to])
-      plot <- plot + ggplot2::coord_sf(xlim=c(bbox$xmin, bbox$xmax),
-                                       ylim=c(bbox$ymin, bbox$ymax))
-  }
-
-
-  plot + labs(title = title)
+    plot + labs(title = title)
 }
 
 #' Creates a Graph Overlay
