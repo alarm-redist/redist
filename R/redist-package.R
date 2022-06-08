bfb58ec135debbf3608ba77f9f342122fda78ff5
Cory McCartan <cory.mccartan@gmail.com>
2022-06-08 13:48:47 -0400

    fix check

--- R/redist-package.R
+++ R/redist-package.R
@@ -37,7 +37,7 @@
 #' @importFrom foreach foreach %do% %dopar%
 #' @importFrom doRNG %dorng%
 #' @importFrom grDevices dev.off pdf
-#' @importFrom stats median runif quantile qnorm IQR optim sd qt var
+#' @importFrom stats median runif quantile qnorm IQR optim sd qt var na.omit
 #' @importFrom utils str
 #' @importFrom dplyr n dplyr_row_slice dplyr_col_modify dplyr_reconstruct .data
 #' @importFrom cli cli_text cli_abort cli_warn cli_inform
