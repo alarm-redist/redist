77df900a444497de189eeacd72436fb18c4e42af
Philip W. O'Sullivan <posullivan.code@gmail.com>
2025-07-12 21:18:01 -0400

    updated merge split tests for new function inputs

--- tests/testthat/test_ms.R
+++ tests/testthat/test_ms.R
@@ -8,7 +8,7 @@ test_that("redist_mergesplit works", {
     expect_equal(range(as.matrix(out)), c(1, 3))
     expect_true(all(par <= 0.1))
 
-    out <- redist_mergesplit(fl_map, 20, 5, thin = 4, init_plan = plans_10[, 1], silent = TRUE)
+    out <- redist_mergesplit(fl_map, 4, 5, thin = 4, init_plan = plans_10[, 1], silent = TRUE)
     expect_equal(ncol(as.matrix(out)), 5L)
 })
 
@@ -24,43 +24,53 @@ test_that("Additional constraints work", {
     plans <- redist_mergesplit(iowa_map, 100, 20, init_plan = iowa$cd_2010,
                                constraints = constr, silent = TRUE)
     skip = seq(1, which.max(by_plan(plans$mcmc_accept, 4)) - 1) # skip to first acceptance
-    expect_false(any(as.matrix(plans)[7, -skip] == 2))
+    # subtract 1 since plans are zero indexed in c++
+    expect_false(any( (as.matrix(plans)[7, -skip] - 1L) == 2))
 })
 
-test_that("redist_mergesplit_parallel works", {
+test_that("redist_mergesplit with multiple chains works", {
     skip_on_os("windows")
     data(fl25)
-    fl_map <- redist_map(fl25, ndists = 3, pop_tol = 0.1) %>% suppressMessages()
+    ndists <- 3
+    fl_map <- redist_map(fl25, ndists = ndists, pop_tol = 0.1) %>% suppressMessages()
 
     N <- 20
     chains <- 2
 
     pl1 <- redist_mergesplit(fl_map, nsims = N, warmup = N/2, chains = chains,
-        ncores = 2, silent = TRUE)
+                                      ncores = 2, silent = TRUE)
     pl2 <- redist_mergesplit(fl_map, nsims = N, chains = chains,
-        ncores = 2, warmup = 0, init_name = F, silent = TRUE)
+                                      ncores = 2, warmup = 0, init_name = FALSE, silent = TRUE)
     pl3 <- redist_mergesplit(fl_map, nsims = N, warmup = N/2, chains = chains,
-        ncores = 2, return_all = F, init_name = F, silent = TRUE)
+                                      ncores = 2, return_all = FALSE, init_name = FALSE, silent = TRUE)
 
     expect_equal(get_n_ref(pl1), chains)
     expect_equal(get_n_ref(pl2), 0)
     expect_equal(get_n_ref(pl3), 0)
 
-    expect_equal(nrow(pl1), 3*chains*(N/2 + 1))
-    expect_equal(nrow(pl2), 3*chains*N)
-    expect_equal(nrow(pl3), 3*chains)
+    # we expect N * chains + num_refs * chains (for the reference plans )
+    expect_equal(nrow(pl1), ndists*chains*N + chains*ndists)
+    expect_equal(nrow(pl2), ndists*chains*N)
+    expect_equal(nrow(pl3), ndists*chains*N)
 })
 
 test_that("Parallel runs are reproducible", {
     set.seed(5118)
-    pl1 <- redist_mergesplit(fl_map, 100, warmup = 50, chains = 2, silent = TRUE)
+    pl1 <- redist_mergesplit(
+        fl_map, 100, warmup = 50, chains = 2, silent = TRUE,
+        control = list(init_ncores = 1L))
+
     set.seed(5118)
-    pl2 <- redist_mergesplit(fl_map, 100, warmup = 50, chains = 2, silent = TRUE)
+    pl2 <- redist_mergesplit(
+        fl_map, 100, warmup = 50, chains = 2, silent = TRUE,
+        control = list(init_ncores = 1L))
 
     # runtime is the only thing that shouldn't be identical
     for (i in 1:2) {
         attr(pl1, "diagnostics")[[i]]$runtime <- NULL
         attr(pl2, "diagnostics")[[i]]$runtime <- NULL
+        attr(pl1, "entire_runtime") <- NULL
+        attr(pl2, "entire_runtime") <- NULL
     }
 
     expect_identical(pl1, pl2)
