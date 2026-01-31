888337f7db2b8dd26ea5dc67238e7dcee0ba5beb
Christopher Kenny <ctk44@cornell.edu>
2026-01-31 13:08:46 -0500

    revert change to map_calc.cpp

--- src/map_calc.cpp
+++ src/map_calc.cpp
@@ -337,10 +337,7 @@ double eval_qps(const subview_col<uword> &districts, int distr,
  */
 double eval_log_st(const subview_col<uword> &districts, const Graph g,
                    arma::uvec counties, int ndists) {
-    // Convert column vector to matrix (required by redistmetrics::log_st_map)
-    arma::umat districts_mat(districts.n_elem, 1);
-    districts_mat.col(0) = districts;
-    return (double) redistmetrics::log_st_map(g, districts_mat, counties, ndists)[0];
+    return (double) redistmetrics::log_st_map(g, districts, counties, ndists)[0];
 }
 
 /*
