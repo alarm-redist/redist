690684f2ece3191c615341f34421f000973419db
Christopher T. Kenny <ctk44@cornell.edu>
2026-02-04 14:39:00 -0500

    Update validate_cyclewalk_7x7.R

--- explore/validate_cyclewalk_7x7.R
+++ explore/validate_cyclewalk_7x7.R
@@ -1,24 +1,56 @@
 devtools::load_all()
 library(sf)
+library(dplyr)
 
 # constants ----
 n <- 7L
 n_chains <- 12L
 
 # targets from Philip ----
-target <- structure(list(edges = c(
-  28, 29, 30, 31, 32, 33, 34, 35, 36,
-  37, 38, 39, 40, 41, 42
-), true_prop = c(
-  0.020807687768601, 0.0710143952315672,
-  0.14977414247499, 0.197857201120775, 0.2074320577516, 0.163717974693393,
-  0.103810429703219, 0.0529758100529919, 0.0222509101447097, 0.0076427174466794,
-  0.00214991687533369, 0.000477190469004618, 7.99810543944961e-05,
-  9.03290674648099e-06, 5.52305514866464e-07
-)), class = c(
-  'tbl_df',
-  'tbl', 'data.frame'
-), row.names = c(NA, -15L))
+target <- structure(
+  list(
+    edges = c(
+      28,
+      29,
+      30,
+      31,
+      32,
+      33,
+      34,
+      35,
+      36,
+      37,
+      38,
+      39,
+      40,
+      41,
+      42
+    ),
+    true_prop = c(
+      0.020807687768601,
+      0.0710143952315672,
+      0.14977414247499,
+      0.197857201120775,
+      0.2074320577516,
+      0.163717974693393,
+      0.103810429703219,
+      0.0529758100529919,
+      0.0222509101447097,
+      0.0076427174466794,
+      0.00214991687533369,
+      0.000477190469004618,
+      7.99810543944961e-05,
+      9.03290674648099e-06,
+      5.52305514866464e-07
+    )
+  ),
+  class = c(
+    'tbl_df',
+    'tbl',
+    'data.frame'
+  ),
+  row.names = c(NA, -15L)
+)
 
 # make shapes ----
 shp <- geomander::checkerboard |>
@@ -44,3 +76,16 @@ plans <- plans |>
   mutate(
     edges_rem = comp_edges_rem(pl(), map)
   )
+
+res <- tibble(
+  edges = unique(plans$edges_rem),
+  est_prop = as.vector(
+    table(
+      plans |> subset_sampled() |> pull(edges_rem)
+    ) /
+      nrow(subset_sampled(plans))
+  )
+)
+
+target |>
+  full_join(res, by = 'edges')
