289fe0ea1260f36e7d5d7da8142f0dcc4102f27b
Christopher Kenny <ctk44@cornell.edu>
2026-02-12 13:17:54 -0500

    remove servr

--- R/enumpart.R
+++ R/enumpart.R
@@ -22,7 +22,7 @@ redist.init.enumpart <- function() {
         writeLines(text = makecontent, con = system.file("enumpart/Makefile", package = "redist"))
     }
 
-    servr::make(dir = system.file("enumpart", package = "redist"), verbose = FALSE)
+    sys::exec_wait("make", args = c("-C", system.file("enumpart", package = "redist")))
 
     # Necessary to avoid bad CRAN submissions:
     if (Sys.info()[["sysname"]] == "Windows") {
