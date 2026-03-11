d2bef6238cd085946505175f3641cbb5b832ea91
Christopher Kenny <ctk44@cornell.edu>
2026-03-10 23:06:06 -0400

    remove k from .h file

--- src/mmss.h
+++ src/mmss.h
@@ -4,7 +4,7 @@
 #include "merge_split.h"
 
 /*
- * Main entry point for Multi-Merge-Split MCMC.
+ * Main entry point for Multiple Merge Sequential Split MCMC.
  *
  * Sample `N` redistricting plans on map `g`, merging and re-splitting
  * `l_merge` districts at each step.  When l_merge=2 this reduces to
@@ -14,7 +14,7 @@ Rcpp::List mmss_plans(int N, List l, const arma::uvec init,
                      const arma::uvec &counties, const arma::uvec &pop,
                      int n_distr, double target, double lower, double upper,
                      double rho, List constraints, List control,
-                     int k, int thin, int l_merge, int verbosity);
+                     int thin, int l_merge, int verbosity);
 
 /*
  * Select `l` connected districts from the district graph via random BFS.
