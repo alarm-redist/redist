ce5f9bb9a0552ba139604f550e2112eab0b04ea2
Christopher Kenny <ctk44@cornell.edu>
2026-07-20 09:26:55 -0400

    fix from @philipwosull

--- src/smc.cpp
+++ src/smc.cpp
@@ -574,6 +574,8 @@ void adapt_parameters(const Graph &g, int &k, int last_k, const vec &lp, double 
             if (districts(j, i) != 0) {
                 ignore[j] = true;
                 n_vtx--;
+            } else {
+                ignore[j] = false;
             }
         }
         if (n_vtx > max_V)
